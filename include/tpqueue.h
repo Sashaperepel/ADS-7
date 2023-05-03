7f3157a48d02f15d820920f72d535f0bfe2acff3
Sashaperepel <124892766+Sashaperepel@users.noreply.github.com>
2023-05-03 11:39:31 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,59 +5,59 @@
 
 template<typename T>
 class TPQueue {
-    private:
-   struct Item {
-      T data;
-      Item* next;
-   };
-   Item* head;
+ private:
+  struct Item {
+    T data;
+    Item* next;
+  };
+  Item* head;
 
-    public:
-   TPQueue() : head(nullptr) {}
-   void push(const T& value) {
-     if (head == nullptr) {
-       Item* temp = new Item;
-       temp->data = value;
-       temp->next = nullptr;
-       head = temp;
-     } else {
-       Item* a1 = head;
-       Item* a2 = new Item;
-       int c = 0;
-       while (a1) {
-         if (a1->data.prior < value.prior) {
-           break;
-         } else {
-           a1 = a1->next;
-           c++;
-         }
-       }
-       if (c == 0) {
-         a2->next = head;
-         a2->data = value;
-         head = a2;
-       } else {
-         a1 = head;
-         for (int i = 1; i < c; i++) {
-           a1 = a1->next;
-         }
-         a2->next = a1->next;
-         a2->data = value;
-         a1->next = a2;
-       }
-     }
-   }
-   T pop() {
-     if (head == nullptr) {
-       throw std::string("Empty");
-     } else {
-       T b = head->data;
-       Item* q = head->next;
-       delete head;
-       head = q;
-       return b;
-     }
-   }
+ public:
+  TPQueue() : head(nullptr) {}
+  void push(const T& value) {
+    if (head == nullptr) {
+      Item* temp = new Item;
+      temp->data = value;
+      temp->next = nullptr;
+      head = temp;
+    } else {
+      Item* a1 = head;
+      Item* a2 = new Item;
+      int c = 0;
+      while (a1) {
+        if (a1->data.prior < value.prior) {
+          break;
+        } else {
+          a1 = a1->next;
+          c++;
+        }
+      }
+      if (c == 0) {
+        a2->next = head;
+        a2->data = value;
+        head = a2;
+      } else {
+        a1 = head;
+        for (int i = 1; i < c; i++) {
+          a1 = a1->next;
+        }
+        a2->next = a1->next;
+        a2->data = value;
+        a1->next = a2;
+      }
+    }
+  }
+  T pop() {
+    if (head == nullptr) {
+      throw std::string("Empty");
+    } else {
+      T b = head->data;
+      Item* q = head->next;
+      delete head;
+      head = q;
+      return b;
+    }
+  }
 };
 
 struct SYM {
